a840e5a9c358d43ba23c523a6c001ccda1628531
NyanCats <reoreo125@gmail.com>
2019-08-03 19:00:33 +0900

    Revert "meow"
    This reverts commit 00f981b615d91e4d538549c9d7874976911d1bb1.

--- BlazorTemplate.Server/Services/AvatarService.cs
+++ BlazorTemplate.Server/Services/AvatarService.cs
@@ -21,8 +21,8 @@ namespace BlazorTemplate.Server.Services
 
         public async Task<bool> CreateAsync(User owner, CancellationToken cancellationToken = default)
         {
-            var userAvatar = await UserAvatarStore.ReadAsync(owner, cancellationToken);
-            if (userAvatar != null) return false;
+            var avatar = await UserAvatarStore.ReadAsync(owner, cancellationToken);
+            if (avatar != null) return false;
 
             var newAvatar = new Avatar()
             {
@@ -31,50 +31,50 @@ namespace BlazorTemplate.Server.Services
                 LastUpdated = DateTime.Now
             };
 
-            var result1 = await UserAvatarStore.CreateAsync(owner, newAvatar, cancellationToken);
-            var result2 = await AvatarStore.CreateAsync(newAvatar, cancellationToken);
-            return result1 && result2;
+            var userAvatarResult = await UserAvatarStore.CreateAsync(owner, newAvatar, cancellationToken);
+            var avatarResult = await AvatarStore.CreateAsync(newAvatar, cancellationToken);
+            return avatarResult && userAvatarResult;
         }
 
         public async Task<byte[]> GetImageAsync(Guid avatarId, CancellationToken cancellationToken = default)
         {
-            var avatar = await AvatarStore.ReadAsync(avatarId, cancellationToken);
-            if (avatar == null) return null;
+            var result = await AvatarStore.ReadAsync(avatarId, cancellationToken);
+            if (result == null) return null;
 
-            return avatar.Image;
+            return result.Image;
         }
 
         public async Task<byte[]> GetImageAsync(User owner, CancellationToken cancellationToken = default)
         {
-            var userAvatar = await UserAvatarStore.ReadAsync(owner, cancellationToken);
-            if (userAvatar == null) return null;
+            var userAvatarResult = await UserAvatarStore.ReadAsync(owner, cancellationToken);
+            if (userAvatarResult == null) return null;
 
-            var avatar = await AvatarStore.ReadAsync(userAvatar.AvatarId);
-            if (avatar == null) throw new Exception();
+            var avatarResult = await AvatarStore.ReadAsync(userAvatarResult.AvatarId);
+            if (avatarResult == null) throw new Exception();
 
-            return avatar.Image;
+            return avatarResult.Image;
         }
 
         public async Task<bool> ExistsAsync(User owner, CancellationToken cancellationToken = default)
         {
-            var userAvatar = await UserAvatarStore.ReadAsync(owner, cancellationToken);
-            if (userAvatar == null) return false;
+            var result = await UserAvatarStore.ReadAsync(owner, cancellationToken);
+            if (result == null) return false;
 
             return true;
         }
 
         public async Task<bool> UpdateAsync(User owner, byte[] image, CancellationToken cancellationToken = default)
         {
-            var userAvatar = await UserAvatarStore.ReadAsync(owner, cancellationToken);
-            if (userAvatar == null) return false;
+            var userAvatarResult = await UserAvatarStore.ReadAsync(owner, cancellationToken);
+            if (userAvatarResult == null) return false;
 
-            var avatar = await AvatarStore.ReadAsync(userAvatar.AvatarId);
-            if (avatar == null) throw new Exception();
+            var avatarResult = await AvatarStore.ReadAsync(userAvatarResult.AvatarId);
+            if (avatarResult == null) throw new Exception();
 
-            avatar.Image = image;
-            avatar.LastUpdated = DateTime.Now;
+            avatarResult.Image = image;
+            avatarResult.LastUpdated = DateTime.Now;
 
-            var result = await AvatarStore.UpdateAsync(avatar, cancellationToken);
+            var result = await AvatarStore.UpdateAsync(avatarResult, cancellationToken);
             return result;
         }
 
